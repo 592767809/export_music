0d7c0adc59fcb4491697a3050e5d16b06ccc06ea
592767809 <592767809@qq.com>
2023-07-06 14:57:18 +0800

    增加汽水音乐解密函数

--- server/qishuimusic.py
+++ server/qishuimusic.py
@@ -1,4 +1,5 @@
 
+import base64
 import re
 import os
 import traceback
@@ -6,10 +7,31 @@ import requests
 import json
 from mysql3.mysql3 import SQL3
 from utils.os import android_listdir, use_song_list, safe_title
+from ExAndroidNativeEmu.androidemu.emulator import Emulator
+from ExAndroidNativeEmu.androidemu.java.java_class_def import JavaClassDef
+from ExAndroidNativeEmu.androidemu.java.java_method_def import java_method_def
+from ExAndroidNativeEmu.androidemu.java.classes.array import Array
+
+
+class Native(metaclass=JavaClassDef, jvm_name='com/ss/ttm/ttvideodecode/Native'):
+
+    def __init__(self):
+        pass
+
+    @java_method_def(name='_getEncryptionKey', signature='([B)Ljava/lang/String;', native=True)
+    def getEncryptionKe(self, mu):
+        pass
+
+    def test(self):
+        pass
 
 
 def app_key_decrypt(spade: str) -> str:
-    return ''
+    emulator = Emulator(vfs_root='ExAndroidNativeEmu/vfs', config_path='ExAndroidNativeEmu/emu_cfg/default.json')
+    emulator.java_classloader.add_class(Native)
+    lib_module = emulator.load_library("so/libvideodec.so")
+    emulator.call_symbol(lib_module, 'JNI_OnLoad', emulator.java_vm.address_ptr, 0x00)
+    return Native().getEncryptionKe(emulator, Array(base64.b64decode(spade.encode()))).get_py_string()
 
 
 def export_file(song_name, song_suffix, song_url, song_encrypt, song_spadea):
@@ -18,7 +40,6 @@ def export_file(song_name, song_suffix, song_url, song_encrypt, song_spadea):
         f.write(song_data)
     if song_encrypt:
         key = app_key_decrypt(song_spadea)
-        assert key
         os.rename('./song/' + song_name + '.' + song_suffix, './song/' + song_name + 'en.' + song_suffix)
         os.system('ffmpeg.exe -decryption_key ' + key + ' -i "' + './song/' + song_name + 'en.' + song_suffix + '" -c copy -copyts -movflags +faststart -movflags +use_metadata_tags -y "' + './song/' + song_name + '.' + song_suffix + '"')
         os.remove('./song/' + song_name + 'en.' + song_suffix)
@@ -39,7 +60,7 @@ def main(ip):
     song_list = []
     for item in values:
         item_info = json.loads(item[1])
-        song_name = item_info['name'] + '-' + '|'.join([each['name'] for each in item_info['artists']])
+        song_name = item_info['name'] + '-' + ','.join([each['name'] for each in item_info['artists']])
         item_info = json.loads(item[3])
         song_suffix = item_info['m_codec_type']
         song_url = item_info['m_main_url']
